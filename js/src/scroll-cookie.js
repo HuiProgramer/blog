469e7d9a47714fa7b44489b6b3fd3d42bd9e7fa5
ProgramerHui <1712817197@qq.com>
2019-02-23 15:24:03 +0800

    Site updated: 2019-02-23 15:23:56

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Wed Feb 20 2019 10:59:00 GMT+0800 (GMT+08:00)
+// build time:Sat Feb 23 2019 13:46:49 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
