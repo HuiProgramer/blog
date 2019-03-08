0b91aec183ea228026b7735dc1c44e79aecfee2b
ProgramerHui <1712817197@qq.com>
2019-03-08 23:19:36 +0800

    Site updated: 2019-03-08 23:19:31

--- js/src/audioPlay.js
+++ js/src/audioPlay.js
@@ -1,3 +1,3 @@
-// build time:Fri Mar 08 2019 20:08:55 GMT+0800 (GMT+08:00)
+// build time:Fri Mar 08 2019 23:19:22 GMT+0800 (GMT+08:00)
 var timeout,rotate=0;var audio=document.getElementById("music1");function startAnim(){timeout=setInterval(function(){var t=document.getElementById("img");var e="rotate("+rotate+"deg)";t.style.transform=e;t.style["-moz-transform"]=e;t.style["-webkit-transform"]=e;t.style["-o-transform"]=e;t.style["-ms-transform"]=e;rotate+=6;if(rotate>360){rotate=0}},30);audio.play()}function stopAnim(){clearInterval(timeout);timeout=null;audio.pause()}startAnim();
 //rebuild by neat 
