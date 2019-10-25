d1da3d1c7b73371b61c2f44311b5fa3d099071dc
ProgramerHui <1712817197@qq.com>
2019-10-25 19:53:22 +0800

    Site updated: 2019-10-25 19:52:51

--- js/src/52share-tips.js
+++ js/src/52share-tips.js
@@ -1,3 +1,3 @@
-// build time:Thu Oct 24 2019 20:52:50 GMT+0800 (GMT+08:00)
+// build time:Fri Oct 25 2019 19:44:12 GMT+0800 (GMT+08:00)
 var wenkmTipsTime=null;var wenkmTips={show:function(e){clearTimeout(wenkmTipsTime);$("#wenkmTips").text(e).addClass("show");this.hide()},hide:function(){wenkmTipsTime=setTimeout(function(){$("#wenkmTips").removeClass("show")},4e3)}};
 //rebuild by neat 
