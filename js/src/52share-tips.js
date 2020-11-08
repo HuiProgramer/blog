adc26607d59c9edc057a65a959ff98991f0398a5
ProgramerHui <1712817197@qq.com>
2020-11-08 22:08:15 +0800

    Site updated: 2020-11-08 22:07:49

--- js/src/52share-tips.js
+++ js/src/52share-tips.js
@@ -1,3 +1,3 @@
-// build time:Sun Oct 18 2020 22:02:52 GMT+0800 (GMT+08:00)
+// build time:Sun Nov 08 2020 21:58:24 GMT+0800 (GMT+08:00)
 var wenkmTipsTime=null;var wenkmTips={show:function(o){clearTimeout(wenkmTipsTime);$("#wenkmTips").text(o).addClass("show");this.hide()},hide:function(){wenkmTipsTime=setTimeout(function(){$("#wenkmTips").removeClass("show")},4e3)}};function b2top(o,t,i){if(10>=t&&0<=t){var n=100*t;$(o).css({backgroundPosition:"0 -"+n+"px"});setTimeout("b2top('"+o+"',"+(i?t+1:t-1)+","+i+")",50)}}$(window).scroll(function(){200<$(this).scrollTop()?$(".backtop").fadeIn():$(".backtop").fadeOut()});$(".backtop").hover(function(){b2top(".backtop",0,!0)},function(){b2top(".backtop",3)}).click(function(){$("body,html").animate({scrollTop:0},600,function(){});b2top(".backtop",3)});
 //rebuild by neat 
