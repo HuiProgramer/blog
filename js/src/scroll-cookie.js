7584e74423321a8ddf19752a1496eb31111750ed
ProgramerHui <1712817197@qq.com>
2021-03-17 19:40:57 +0800

    Site updated: 2021-03-17 19:40:02

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Fri Mar 05 2021 23:33:02 GMT+0800 (GMT+08:00)
+// build time:Wed Mar 17 2021 19:21:44 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
